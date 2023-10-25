8fa4a6875fbc5410afcd72f008de48aea34d40f7
student <sbs.alexlips@gmail.com>
2023-10-25 16:43:23 +0200

    css erstellt für heroSection und an heroSection.jsx was geändert

--- components/landingPage/heroSection.jsx
+++ components/landingPage/heroSection.jsx
@@ -1,17 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
+import styles from './heroSection.module.css';
 
 export default function HeroSection() {
-
   return (
-<div style={{ display: 'flex' }}>
-  <div id="abteilung1" style={{ flex: 1 }}>
-    <h2>Abteilung 1</h2>
-    <p>Dies ist der Inhalt der ersten Abteilung.</p>
-  </div>
-  <div id="abteilung2" style={{ flex: 1 }}>
-    <h2>Abteilung 2</h2>
-    <p>Dies ist der Inhalt der zweiten Abteilung.</p>
-  </div>
-</div>
-  );her
+    <div className={styles.heroSectionbackground}>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <h2>Abteilung 1</h2>
+          <p>Dies ist der Inhalt der ersten Abteilung.</p>
+        </div>
+        <div className={styles.content}>
+          <h2>Abteilung 2</h2>
+          <p>Dies ist der Inhalt der zweiten Abteilung.</p>
+        </div>
+      </div>
+    </div>
+  );
 }
