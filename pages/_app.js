99aaf4676594815abb29040aeccdf5d641378e23
supertobi2001@gmail.com <16t1017E>
2023-10-25 11:14:37 +0200

    changed the backgorund image

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import Layout from "@/components/staticComponents/Layout";
 import '/styles/navigation.css'
 import '/styles/FunctionInfo.css'
+import 'styles/globals.css'
 
 
 
