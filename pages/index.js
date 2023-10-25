de554230beb99e1d92d8a3a8e16502ad533e7156
student <sbs.alexlips@gmail.com>
2023-10-25 09:36:19 +0200

    index seite geändert

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-
+import heroSection from "@/components/landingPage/heroSection";
 export default function Home() {
 
   return (
     <div>
-        HomePage1
+        <heroSection/>
     </div>
   );
 }
