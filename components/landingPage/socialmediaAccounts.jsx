feaec11493fa6b9cb601ea9713ccbc77f263ac0d
student <d.ojimba@web.de>
2023-10-25 20:49:16 +0200

    CSS socialmediaAccounts.css styling

--- components/landingPage/socialmediaAccounts.jsx
+++ components/landingPage/socialmediaAccounts.jsx
@@ -5,35 +5,35 @@ export default function SocialmediaAccounts() {
   return (
     <Container className="text-center">
       <h1>Verbinde deine Social Media Accounts</h1>
-      <p>Wir unterstützen diese Plattformen</p>
+      <p className="semi-transparent-text">Wir unterstützen diese Plattformen</p>
       <Row className="justify-content-center">
         <Col>
-          <div className="social-icon">
+          <div className="social-icon mt-3">
             <img src="/facebookLogoIcon.png" alt="Icon 1" />
           </div>
         </Col>
         <Col>
-          <div className="social-icon">
+          <div className="social-icon mt-3">
             <img src="/instagramLogoIcon.png" alt="Icon 2" />
           </div>
         </Col>
         <Col>
-          <div className="social-icon">
+          <div className="social-icon mt-3">
             <img src="/whatsappLogoIcon.png" alt="Icon 3" />
           </div>
         </Col>
         <Col>
-          <div className="social-icon">
+          <div className="social-icon mt-3">
             <img src="/tiktokLogoIcon.png" alt="Icon 4" />
           </div>
         </Col>
         <Col>
-          <div className="social-icon">
+          <div className="social-icon mt-3">
             <img src="/youtubeLogoIcon.png" alt="Icon 5" />
           </div>
         </Col>
       </Row>
-      <Button variant="primary">Erfahre mehr</Button>
+      <Button className="sma-button mt-4">Erfahre mehr</Button>
     </Container>
   );
 }
