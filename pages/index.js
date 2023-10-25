58f90c346b6ab38887a4670be733a4ce44ce6f48
student <sbs.alexlips@gmail.com>
2023-10-25 09:38:33 +0200

    index seite geändert

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import carousel from "@/components/landingPage/carousel";
-
+import heroSection from "@/components/landingPage/heroSection";
 export default function Home() {
 
   return (
     <div>
         <carousel/>
+        <heroSection/>
     </div>
   );
 }
