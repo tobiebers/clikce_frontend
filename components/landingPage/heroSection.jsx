4daa0eaaf3a677ed136551cc3e13a3de5da18643
student <sbs.alexlips@gmail.com>
2023-10-25 21:16:21 +0200

    css erstellt für heroSection und an heroSection.jsx was geändert

--- components/landingPage/heroSection.jsx
+++ components/landingPage/heroSection.jsx
@@ -4,29 +4,27 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 export default function HeroSection() {
   return (
-    <div className="heroSectionbackground">
-      <Container>
+      <Container className="heroSectionbackground">
         <Row>
-          <Col md={6}>
-            <div className="content">
-              <h2 className="custom-h2">Erfolg in Sozialen</h2>
-              <h2 className="custom-h2">Medien - Wir machen</h2>
-              <h2 className="custom-h2">es möglich!</h2>
-              <p className="custom-h2">
+          <Col className="col-centered" md={6}>
+            <div>
+              <h2>Erfolg in Sozialen</h2>
+              <h2>Medien - Wir machen</h2>
+              <h2>es möglich!</h2>
+              <p>
                 Strategie, Analyse und Umsetzung:<br />
                 Entdecken Sie, wie unser umfassendes Social Media Management<br />
                 Ihre Socialmediapräsenz nach vorne bringt!
               </p>
-              <button className="button1">Register</button>
+              <button>Register</button>
             </div>
           </Col>
-          <Col md={6}>
-            <div className="imageContainer">
+          <Col className="col-centered" md={6}>
+            <div>
               <img src="/heroSection.png" width="270" height="270" />
             </div>
           </Col>
         </Row>
       </Container>
-    </div>
   );
 }
