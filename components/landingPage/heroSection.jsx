832e62152834bdcc66c9ede1ffa89725e0db7c84
student <sbs.alexlips@gmail.com>
2023-10-25 16:24:49 +0200

    herosection geändert

--- components/landingPage/heroSection.jsx
+++ components/landingPage/heroSection.jsx
@@ -3,8 +3,15 @@ import React, { useState } from 'react';
 export default function HeroSection() {
 
   return (
-    <div>
-        Hero Section
-    </div>
-  );
+<div style={{ display: 'flex' }}>
+  <div id="abteilung1" style={{ flex: 1 }}>
+    <h2>Abteilung 1</h2>
+    <p>Dies ist der Inhalt der ersten Abteilung.</p>
+  </div>
+  <div id="abteilung2" style={{ flex: 1 }}>
+    <h2>Abteilung 2</h2>
+    <p>Dies ist der Inhalt der zweiten Abteilung.</p>
+  </div>
+</div>
+  );her
 }
