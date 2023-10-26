c126b334edda2618888efa03a447f26a29a38554
student <sbs.alexlips@gmail.com>
2023-10-26 09:47:52 +0200

    fußzeile geändert

--- components/staticComponents/Footer.jsx
+++ components/staticComponents/Footer.jsx
@@ -1,7 +1,10 @@
+import Image from 'public/logo.png';
+
 export default function Footer() {
   return (
-    <div className='d-flex justify-content-center fixed-bottom text-secondary bg-light'>
-      <h6>Das ist die Fußzeile  </h6>
+    <div className='d-flex justify-content-between align-items-center fixed-bottom text-secondary' style={{ backgroundColor: 'rgba(214, 235, 234, 0.7)' }}>
+      <h6>© 2023 CLIKCE. All rights Reserved. <a href="/privacy-policy">Privacy Policy</a></h6>
+      <img src="logo.png" alt="Your Image" width={80} height={80} className="img-fluid" />
     </div>
-  )
+  );
 }
