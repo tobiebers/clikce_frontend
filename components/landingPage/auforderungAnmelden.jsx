3d7ccfb219d3cc152f3d7aec772e1caa26109721
student <sbs.alexlips@gmail.com>
2023-10-27 09:34:05 +0200

    abstände geändert

--- components/landingPage/auforderungAnmelden.jsx
+++ components/landingPage/auforderungAnmelden.jsx
@@ -7,8 +7,8 @@ export default function HeroSection() {
         <Row className="background-color-white m-2 rounded-45">
           <Col className="col-centered" md={6}>
             <div>
-              <h2 className="custom-line-height mt-4">Melde dich jetzt an!</h2>
-              <p className="mt-4 text-m">
+              <h2 className="custom-line-height mt-5">Melde dich jetzt an!</h2>
+              <p className="mt-5 text-m">
                 Entdecken Sie jetzt, wie unser umfassendes Social Media<br/>
                 Management Ihre<br />
                 Ihre Socialmediapräsenz nach vorne bringt!
