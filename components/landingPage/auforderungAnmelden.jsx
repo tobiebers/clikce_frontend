0fa70395d5e3e582fc31edae71de3b9f712969a6
student <sbs.alexlips@gmail.com>
2023-10-27 09:11:04 +0200

    auforderungAnmelden geändert und globals.css button hinzugefügt

--- components/landingPage/auforderungAnmelden.jsx
+++ components/landingPage/auforderungAnmelden.jsx
@@ -1,42 +1,27 @@
 import React from 'react';
-import { Container, Button } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
-export default function RegistrationComponent() {
+export default function HeroSection() {
   return (
-    <div
-      style={{
-        background: 'white',
-        borderRadius: '10px', // Radius anpassen, wie gewünscht
-        padding: '20px',
-        position: 'relative',
-      }}
-    >
-      <h1 className="text-right" style={{ position: 'relative', zIndex: 1 }}>
-        Melde dich jetzt an!
-      </h1>
-      <p className="text-right" style={{ position: 'relative', zIndex: 1 }}>
-        Entdecken Sie jetzt, wie unser umfassendes Social Media Management<br />Ihre Socialmediapräsenz nach vorne bringt
-      </p>
-      <Button
-        variant="dark"
-        className="float-right"
-        style={{ position: 'relative', zIndex: 1 }}
-      >
-        Register
-      </Button>
-
-      <img
-        src="Login-pana.png"
-        alt="Bild"
-        style={{
-          position: 'absolute',
-          right: '0',
-          top: '-20px', // Anpassen, um 20px über den oberen Rand hinauszuragen
-          width: 'auto',
-          height: 'calc(160% + 40px)', // Erhöht die Höhe um 40px und ragt oben 20px über den Rand
-          zIndex: 0, // Setzt den Stapelwert auf 0, um das Bild über dem Text und dem Button zu legen
-        }}
-      />
-    </div>
+      <Container>
+        <Row className="background-color-white m-2 rounded-45">
+          <Col className="col-centered" md={6}>
+            <div>
+              <h2 className="custom-line-height mt-4">Melde dich jetzt an!</h2>
+              <p className="mt-4 text-m">
+                Entdecken Sie jetzt, wie unser umfassendes Social Media<br/>
+                Management Ihre<br />
+                Ihre Socialmediapräsenz nach vorne bringt!
+              </p>
+              <button className="btn4 button-small">Register</button>
+            </div>
+          </Col>
+          <Col className="col-centered" md={6}>
+            <div>
+              <img src="/Login-pana.png" width="250" height="250" />
+            </div>
+          </Col>
+        </Row>
+      </Container>
   );
 }
