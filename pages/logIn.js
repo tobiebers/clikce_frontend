d0c27be578513e2d67c8d15454bf0d238b743c8a
student <sbs.alexlips@gmail.com>
2023-10-27 10:12:22 +0200

    logIn pages erstellt

---
+++ pages/logIn.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import logIn from "@/components/loginPage/logIn.jsx";
+export default function Home() {
+
+  return (
+    <div>
+    <logIn/>
+    </div>
+  );
+}
