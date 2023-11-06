1ad308ca1a4f2de3372576e6c437560506e0d3f9
student <sbs.alexlips@gmail.com>
2023-11-06 12:09:05 +0100

    logIn pages geändert

--- components/loginPage/logIn.jsx
+++ components/loginPage/logIn.jsx
@@ -4,17 +4,24 @@ import { Container, Row, Col } from 'react-bootstrap';
 export default function Login() {
   return (
     <Container>
-      <Row style={{ height: '600px', backgroundColor: '#D6EBEA' }}>
+      <Row noGutters style={{ height: '600px', backgroundColor: '#D6EBEA' }}>
         <Col md={5}>
-          {/* Hier kannst du den Inhalt für das 2/3 Rechteck hinzufügen */}
+        <div>
+          <img className="img-size-logologin" src="logo.png" />
+        </div>
         </Col>
         <Col md={7} style={{ backgroundColor: '#4B8E94' }}>
-          {/* Hier kannst du den Inhalt für das 1/3 Rechteck hinzufügen */}
+          <div>
+            <img className="img-size-logIn" src="LogInimage.png" alt="Bildbeschreibung" />
+          </div>
         </Col>
       </Row>
     </Container>
   );
 }
+
+
+
 
 
 
