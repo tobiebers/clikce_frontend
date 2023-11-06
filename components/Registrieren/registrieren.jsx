1522297b1d1ef0ad84a4674798b6cf8bcb7addcd
student <d.ojimba@web.de>
2023-11-06 13:00:15 +0100

    StyleSheet mit Register.css hinzugefügt, Directory mit RegisterPage hinzugefügt, anshcließend gleich die JSX Datei register.jsx mit eingefügt

---
+++ components/Registrieren/registrieren.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+export default function Login() {
+  return (
+    <Container>
+      <Row noGutters style={{ height: '600px', backgroundColor: '#D6EBEA' }}>
+        <Col md={5}>
+          <div>
+            <img className="img-size-logologin" src="logo.png" />
+            <div className="img-size-googleandfacebook-container">
+              <img className="img-size-googleandfacebook" src="facebook.png" alt="Facebook" />
+              <img className="img-size-googleandfacebook" src="google.png" alt="Google" />
+            </div>
+          </div>
+        </Col>
+        <Col md={7} style={{ backgroundColor: '#4B8E94' }}>
+          <div>
+            <img className="img-size-logIn" src="register.png" alt="Bildbeschreibung" />
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+
+
+
+
+
+
+
---
+++ pages/registrieren.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import Registrieren from "@/components/Registrieren/registrieren";
+export default function Home() {
+
+  return (
+    <div>
+    <Registrieren/>
+    </div>
+  );
+}
