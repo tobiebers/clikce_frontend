7fc17df19cdd0ce733026a458e6418cd55934ab9
MaaB0I <dieterrorhenne@gmail.com>
2023-11-14 14:17:45 +0100

    Carousel bearbeitet, Personal Info site added, register tryed to change but did not work

---
+++ pages/personal_info.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import personal_info from "@/components/personal info/personal_info";
+import PersonalInfo from "@/components/personal info/personal_info";
+export default function Home() {
+
+  return (
+    <div>
+    <PersonalInfo/>
+    </div>
+  );
+}
