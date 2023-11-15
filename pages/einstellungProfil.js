ce61922def6487df4980acc0f5e60c6af65f923b
student <sbs.alexlips@gmail.comconfig --global user.name alexgit config --global user.email sbs.alexlips@gmail.com>
2023-11-15 10:37:11 +0100

    EinstellungProfil erstellt

---
+++ pages/einstellungProfil.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import einstellungProfil from 'components/einstellungProfil/einstellungProfil';
+export default function Home() {
+
+  return (
+    <div>
+    <einstellungProfil/>
+    </div>
+  );
+}
