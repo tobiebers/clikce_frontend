3d5640ab7da362e8be8197a30163f4aa3e89e9d0
student <sbs.alexlips@gmail.comconfig --global user.name alexgit config --global user.email sbs.alexlips@gmail.com>
2023-11-15 18:10:29 +0100

    EinstellungProfil erstellt

--- components/einstellungProfil/einstellungProfil.jsx
+++ components/einstellungProfil/einstellungProfil.jsx
@@ -7,10 +7,25 @@ export default function einstellungProfil() {
       <Row noGutters style={{ height: '600px', backgroundColor: '#D6EBEA' }}>
         <Col md={5} className="" style={{ textAlign: 'center' }}>
           <div>
+            <h1 className="transparent-text">Persönliche Daten</h1>
+            <p className="transparent-text">Firstname:</p>
+            <p className="transparent-text">Lastname:</p>
+            <p className="transparent-text">Branche:</p>
+            <p className="transparent-text">Sprache:</p>
+            <p className="transparent-text">Passwort:</p>
+            <p className="transparent-text">Ziele:</p>
+            <p className="transparent-text">Kurze Beschreibung:</p>
           </div>
         </Col>
         <Col md={7} style={{ backgroundColor: '#4B8E94' }}>
           <div>
+            <input type="text" className="white-input input-text" style={{ width: '100%' }} />
+            <input type="text" className="white-input input-text" style={{ width: '100%' }} />
+            <input type="text" className="white-input input-text" style={{ width: '100%' }} />
+            <input type="text" className="white-input input-text" style={{ width: '100%' }} />
+            <input type="text" className="white-input input-text" style={{ width: '100%' }} />
+            <input type="text" className="white-input input-text" style={{ width: '100%' }} />
+            <input type="text" className="white-input input-text" style={{ width: '100%' }} />
           </div>
         </Col>
       </Row>
