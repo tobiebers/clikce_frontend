73af44e57a3c12197658e8a6adbf27187e60ba76
MaaB0I <dieterrorhenne@gmail.com>
2023-11-17 15:07:17 +0100

    Update carousell (still not working), Try to fix Footer (not fixed), Kontakt Page connected to Backend (worked)

--- components/landingPage/MyCarouselComponent.jsx
+++ components/landingPage/MyCarouselComponent.jsx
@@ -1,28 +1,60 @@
 import React from 'react';
 
-const CarouselComponent = () => {
+export default function MyCarousel() {
   return (
-    <div className="void" id="void">
-      <div className="crop">
-        <ul id="card-list" style={{ '--count': 6 }}>
-          {Array.from({ length: 6 }).map((_, idx) => (
-            <li key={idx}>
-              <div className="card">
-                <a href="">
-                  <span className="model-name">Gretel-ACTGAN</span>
-                  <span>Model for generating highly dimensional, mostly numeric, tabular data</span>
-                </a>
-              </div>
-            </li>
-          ))}
-        </ul>
-        <div className="last-circle"></div>
-        <div className="second-circle"></div>
+    <div id="myCarousel" className="carousel slide" data-ride="carousel">
+      {/* Indicators */}
+      <ol className="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+        <li data-target="#myCarousel" data-slide-to="3"></li>
+      </ol>
+
+      {/* Wrapper for slides */}
+      <div className="carousel-inner" role="listbox">
+        <div className="item active">
+          <img src="google.png" alt="Chania" />
+          <div className="carousel-caption">
+            <h3>Chania</h3>
+            <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+          </div>
+        </div>
+
+        <div className="item">
+          <img src="google.png" alt="Chania" />
+          <div className="carousel-caption">
+            <h3>Chania</h3>
+            <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+          </div>
+        </div>
+
+        <div className="item">
+          <img src="google.png" alt="Flower" />
+          <div className="carousel-caption">
+            <h3>Flowers</h3>
+            <p>Beautiful flowers in Kolymbari, Crete.</p>
+          </div>
+        </div>
+
+        <div className="item">
+          <img src="google.png" alt="Flower" />
+          <div className="carousel-caption">
+            <h3>Flowers</h3>
+            <p>Beautiful flowers in Kolymbari, Crete.</p>
+          </div>
+        </div>
       </div>
-      <div className="mask"></div>
-      <div className="center-circle"></div>
+
+      {/* Left and right controls */}
+      <a className="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span className="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span className="sr-only">Previous</span>
+      </a>
+      <a className="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span className="sr-only">Next</span>
+      </a>
     </div>
   );
 }
-
-export default CarouselComponent;
--- components/staticComponents/Footer.jsx
+++ components/staticComponents/Footer.jsx
@@ -1,10 +1,14 @@
+import React from 'react';
 import Image from 'public/logo.png';
 
-export default function Footer() {
+const Footer = () => {
   return (
-    <div className='d-flex justify-content-between align-items-center text-secondary' style={{ backgroundColor: 'rgba(214, 235, 234, 0.7)' }}>
+    <div className='d-flex justify-content-between align-items-center text-secondary'
+         style={{ backgroundColor: 'rgba(214, 235, 234, 0.7)' }}>
       <p className="text-s">© 2023 CLIKCE. All rights Reserved. <a href="/privacy-policy">Privacy Policy</a></p>
-      <img src="logo.png" alt="Your Image" width={80} height={80} className="img-fluid" />
+      <img src="logo.png" alt="Logo" width={80} height={80} className="img-fluid" />
     </div>
   );
-}
+};
+
+export default Footer;
