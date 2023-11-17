23a24e6056dc5b2f5cfc673d9b69381bda8e15ee
DavidOji <d.ojimba@web.de>
2023-11-17 13:33:33 +0100

    StyleSheet mit pricing.css.css hinzugefügt, Directory mit pricing.jsx hinzugefügt, anschließend gleich die JSX Datei pricing.js mit eingefügt. Programmieren angefangen

--- components/Registrieren/registrieren.jsx
+++ components/Registrieren/registrieren.jsx
@@ -7,7 +7,7 @@ export default function Registrieren() {
       <Row noGutters style={{ height: '600px', backgroundColor: '#D6EBEA' }}>
         <Col md={5} className="d-flex flex-column align-items-start justify-content-center">
           <div>
-            <img className="img-size-logologin" src="logo.png" />
+            <img className="img-size-logologin mt-0" src="logo.png" />
             <div className="img-size-googleandfacebook-container">
               <img className="img-size-googleandfacebook" src="facebook.png" alt="Facebook" />
               <img className="img-size-googleandfacebook" src="google.png" alt="Google" />
@@ -16,42 +16,46 @@ export default function Registrieren() {
 
             {/* Fullname Textfeld */}
             <div className="text-container">
-              <p className="text-style transparent-bg">Fullname</p>
-              <input type="text" className="white-input" />
+              <p className="text-style transparent-bg">Vor/- Nachname</p>
+              <input type="text" className="w-input" />
             </div>
 
             {/* E-mail Textfeld */}
-            <div className="text-container">
+            <div className="text-container mt-2">
               <p className="text-style transparent-bg">E-mail</p>
-              <input type="text" className="white-input" />
+              <input type="text" className="w-input" />
             </div>
 
             {/* Username Textfeld */}
-            <div className="text-container">
+            <div className="text-container mt-2">
               <p className="text-style transparent-bg">Username</p>
-              <input type="text" className="white-input" />
+              <input type="text" className="w-input" />
             </div>
 
             {/* Password Textfeld */}
-            <div className="text-container">
-              <p className="text-style transparent-bg">Password</p>
-              <input type="password" className="white-input" />
+            <div className="text-container mt-2">
+              <p className="text-style transparent-bg">Passwort</p>
+              <input type="password" className="w-input" />
             </div>
 
             {/* Checkbox unter den Textfeldern */}
-            <div className="checkbox-container text-center" style={{ marginTop: '0px' }}>
+            <div className="checkbox-container text-center" style={{ marginTop: '20px' }}>
               <input type="checkbox" id="termsCheckbox" />
               <label htmlFor="termsCheckbox" className="checkbox-label smaller-text">
-                By creating an account you agree to the<br />
-                <span className="terms-of-use">terms of use</span> and our&nbsp;
-                <span className="privacy-policy">privacy policy</span>.
+                Durch die Erstellung eines Kontos stimmen Sie den<br />
+                <span className="terms-of-use">Nutzungsbedingungen</span> und unseren&nbsp; <br />
+                <span className="privacy-policy">Datenschutzrichtlinie </span> zu.
               </label>
             </div>
-            <button className="mt-3 btn6 button-small">Create Account</button>
+
+            {/* Button in der Mitte positionieren */}
+            <div className="flex-md-row-reverse text-center">
+              <button className="mt-3 btn6 button-small">Account erstellen</button>
+            </div>
 
             {/* Already have an account? Log in Text */}
-            <p className="small-text text-center mt-1">
-              Already have an account?{' '}
+            <p className="small-text text-center me-2">
+              Haben Sie bereits einen Account?{' '}
               <span className="login-text" style={{ color: '#FFB3B8' }}>
                 Log In
               </span>
