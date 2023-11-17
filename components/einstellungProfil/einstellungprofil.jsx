5eae6eadf978e7c9e044292e87f9f0b9b3cd72f0
student <sbs.alexlips@gmail.comconfig --global user.name alexgit config --global user.email sbs.alexlips@gmail.com>
2023-11-17 15:00:50 +0100

    EinstellungProfil erstellt

--- components/einstellungProfil/einstellungprofil.jsx
+++ components/einstellungProfil/einstellungprofil.jsx
@@ -1,7 +1,40 @@
 import React, { useState, useRef } from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import { SERVER_URL } from '../../config.js';
 
 export default function Einstellungprofil() {
+  const [vorname, setVorname] = useState('');
+  const [nachname, setNachname] = useState('');
+  const [branche, setBranche] = useState('');
+  const [sprache, setSprache] = useState('');
+  const [password, setPassword] = useState('');
+  const [ziele, setZiele] = useState('');
+  const [beschreibung, setBeschreibung] = useState('');
+
+    const handleEinstellungprofil = async () => {
+    const response = await fetch(`${SERVER_URL}/einstellungProfil`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+      body: JSON.stringify({
+        vorname: vorname,
+        nachname: nachname,
+        branche: branche,
+        sprache: sprache,
+        password: password,
+        ziele: ziele,
+        beschreibung: beschreibung,
+
+      }),
+    });
+
+    const data = await response.json();
+    console.log(data); // Zeigt die Antwort des Servers
+  };
+
+
+
   const formRef1 = useRef();
   const formRef2 = useRef();
 
@@ -54,13 +87,24 @@ export default function Einstellungprofil() {
         <Col md={7} style={{ backgroundColor: '#4B8E94' }}>
           <div>
             <div className="input-container abstand-oben">
-              <input type="text" className="white-inputeinstellungProfil input-text"/>
+              <input
+                  type="text"
+                  className="white-inputeinstellungProfil input-text"
+                  value={vorname}
+                  onChange={(e) => setVorname(e.target.value)}
+              />
             </div>
             <div className="input-container abstand-dazwischen">
-              <input type="text" className="white-inputeinstellungProfil input-text"/>
+              <input
+                  type="text"
+                  className="white-inputeinstellungProfil input-text"
+                  value={nachname}
+                  onChange={(e) => setNachname(e.target.value)}
+              />
             </div>
             <Form ref={formRef1} onSubmit={handleSubmit}>
-              <Form.Group className="abstand-dazwischen2" controlId="formTopic1">
+              <Form.Group className="abstand-dazwischen2" controlId="formTopic1" value={branche}
+                  onChange={(e) => setBranche(e.target.value)}>
                 <Form.Select className="white-input-dropdown dropdown-text" name="topic1" value={formData.topic} onChange={handleChange}>
                   <option value="">Wähle eine Branche</option>
                   <option value="Influencer">Influencer</option>
@@ -71,11 +115,11 @@ export default function Einstellungprofil() {
                   <option value="other">Other</option>
                 </Form.Select>
               </Form.Group>
-              {/* Add the rest of your form components here */}
             </Form>
 
             <Form ref={formRef2} onSubmit={handleSubmit}>
-            <Form.Group className="abstand-dazwischen2" controlId="formTopic2">
+            <Form.Group className="abstand-dazwischen2" controlId="formTopic2" value={sprache}
+                  onChange={(e) => setSprache(e.target.value)}>
               <Form.Select className="white-input-dropdown dropdown-text" name="topic2" value={formData.topic} onChange={handleChange}>
                 <option value="">Wähle eine Sprache</option>
                 <option value="technicalSupport">Deutsch</option>
@@ -89,19 +133,28 @@ export default function Einstellungprofil() {
           </Form>
 
             <div className="input-container abstand-dazwischen">
-              <input type="password" className="white-inputeinstellungProfil input-text abstand-dazwischen"/>
+              <input
+                  type="password"
+                  className="white-inputeinstellungProfil input-text abstand-dazwischen"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
             <textarea
               type="Beschreibung"
               className="white-inputZiele input-text abstand-dazwischen2"
+              value={ziele}
+                  onChange={(e) => setZiele(e.target.value)}
             />
             <div className="input-container abstand-dazwischen">
               <textarea
                 type="Beschreibung"
                 className="white-inputZiele input-text abstand-dazwischen"
+                value={beschreibung}
+                  onChange={(e) => setBeschreibung(e.target.value)}
               />
             </div>
-            <Button className="btn5" type="submit">
+            <Button className="btn5" type="submit" onClick={handleEinstellungprofil}>
               Speichern
             </Button>
           </div>
