4180e818e9b959a39ae05cad792b996670f4a8f9
DavidOji <d.ojimba@web.de>
2023-11-27 10:28:57 +0100

    Groß/Klein Schreibung

--- components/landingPage/socialmediaAccounts.jsx
+++ components/landingPage/socialmediaAccounts.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 
-export default function SocialMediaAccounts() {
+export default function SocialmediaAccounts() {
   return (
     <Container className="text-center">
       <h1>Verbinde deine Social Media Accounts</h1>
