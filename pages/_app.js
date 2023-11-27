84ec0bf1ab63dbc272626113d4f6318f6bf44bb1
supertobi2001@gmail.com <16t1017E>
2023-11-27 09:47:07 +0100

    Groß und Kleinschreibung

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { useEffect } from 'react';
-import Layout from "@/components/staticComponents/Layout";
+import Layout from "@/components/static-components/Layout";
 import '/styles/navigation.css'
 import '/styles/functionInfo.css'
 import 'styles/globals.css'
