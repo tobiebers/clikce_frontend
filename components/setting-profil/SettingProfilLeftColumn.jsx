aea0720e9a8db1b4dd7be399c2a5cac3b60e4df5
student <sbs.alexlips@gmail.comconfig --global user.name alexgit config --global user.email sbs.alexlips@gmail.com>
2023-11-28 09:37:29 +0100

    settinProfil design angepasst

--- components/setting-profil/SettingProfilLeftColumn.jsx
+++ components/setting-profil/SettingProfilLeftColumn.jsx
@@ -3,14 +3,14 @@ import React from "react";
 export default function SettingProfilLeftColumn() {
   return (
         <div>
-            <p className="transparent-text persönlicheDaten-text abstand-links">Persönliche Daten</p>
-            <p className="transparent-text abstand-links">Vorname:</p>
-            <p className="transparent-text abstand-links">Nachname:</p>
-            <p className="transparent-text abstand-links">Branche:</p>
-            <p className="transparent-text abstand-links">Sprache:</p>
-            <p className="transparent-text abstand-links">Passwort:</p>
-            <p className="transparent-text abstand-links">Ziele:</p>
-            <p className="transparent-text abstand-links abstand-zuZiele">Kurze Beschreibung:</p>
+            <p className="transparent-text persönlicheDaten-text">Persönliche Daten</p>
+            <p className="transparent-text">Vorname:</p>
+            <p className="transparent-text">Nachname:</p>
+            <p className="transparent-text">Branche:</p>
+            <p className="transparent-text">Sprache:</p>
+            <p className="transparent-text">Passwort:</p>
+            <p className="transparent-text">Ziele:</p>
+            <p className="transparent-text abstand-zuZiele">Kurze Beschreibung:</p>
         </div>
   );
 }
