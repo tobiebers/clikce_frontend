7f7434b7c6c6113a55d92098361071c1b302d92e
supertobi2001@gmail.com <16t1017E>
2023-12-01 10:58:08 +0100

    Authprovider erstellt um globalen anmeldezustand zu überprüfen

--- pages/_app.js
+++ pages/_app.js
@@ -13,8 +13,8 @@ import '@/styles/contact.css'
 import '@/styles/settingProfil.css'
 import '@/styles/app/verticalNavigation.css'
 import '@/styles/app/questions.css'
-import {AuthProvider} from "@/components/out-app/static-components/AuthContext";
 
+import {AuthProvider} from "@/components/out-app/static-components/AuthContext";
 import { useAuth } from "@/components/out-app/static-components/AuthContext";
 
 
