84cd0a0a1ca10242209638088a405a22e9ece00c
supertobi2001@gmail.com <16t1017E>
2023-12-08 12:43:02 +0100

    Chart geupdatet

--- components/in-app/dashboard/PieChart1.jsx
+++ components/in-app/dashboard/PieChart1.jsx
@@ -1,61 +1,57 @@
 import React, { useState, useEffect } from 'react';
 import { PieChart, Pie, Cell, Tooltip, Legend } from 'recharts';
 
+const PieChart1 = () => {
+  const [data, setData] = useState([]);
 
-export default function PieChart1() {
-    const [data, setData] = useState(0);
+  const COLORS = ['#7E57C2', '#0e9692', '#FF81AE', '#ee031e'];
 
-    useEffect(() => {
-        const fetchChartPie = async () => {
-      try {
-        const response = await fetch('http://localhost:5000/fetch-chart-data');
+  useEffect(() => {
+    // API aufrufen, um Daten zu erhalten
+    fetch('http://localhost:5000/fetch-chart-data')
+      .then(response => {
         if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
+          throw new Error('Netzwerkantwort war nicht ok');
         }
-    const json= await response.json();
-
-        // Anpassung der Datenformatierung entsprechend den Backend-Schlüsseln
-        const fetchedData = Object.entries(json).map(([key, value]) => ({
-          name: key.replace('wert', ''), // Entferne 'wert' aus dem Schlüssel
-          value: value
-        }));(fetchedData);
-
-      } catch (error) {
-        console.error('Fehler beim Abrufen der Daten vom Backend:', error);
-      }
-    };
-
-    fetchChartPie();
+        return response.json();
+      })
+      .then(fetchedData => {
+        // Umwandlung der Daten in das benötigte Format
+        const formattedData = Object.keys(fetchedData).map(key => ({
+          name: key,
+          value: fetchedData[key]
+        }));
+        setData(formattedData);
+      })
+      .catch(error => console.error('Fehler beim Abrufen der Daten:', error));
   }, []);
 
-    const COLORS = ['#7E57C2', '#0e9692', '#FF81AE', '#ee031e'];
-
   return (
-    <div className="mt-3">
-        <div style={{ position: 'relative' }}>
-          <h3 className="follower-text">
-            Follower
-          </h3>
-          <PieChart width={400} height={330} className="background-color-secondary borderChart">
-            <Pie
-              data={data}
-              cx={200}
-              cy={165}
-              innerRadius={60}
-              outerRadius={80}
-              fill="#8884d8"
-              paddingAngle={5}
-              dataKey="value"
-              cornerRadius={10}
-            >
-              {data.map((entry, index) => (
-                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-              ))}
-            </Pie>
-            <Tooltip />
-            <Legend />
-          </PieChart>
-        </div>
+    <div>
+      <div style={{ position: 'relative' }}>
+        <h3 className="follower-text">Follower</h3>
+        <PieChart width={400} height={330} className="background-color-secondary borderChart">
+          <Pie
+            data={data}
+            cx={200}
+            cy={165}
+            innerRadius={60}
+            outerRadius={80}
+            fill="#8884d8"
+            paddingAngle={5}
+            dataKey="value"
+            cornerRadius={10}
+          >
+            {data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Pie>
+          <Tooltip />
+          <Legend />
+        </PieChart>
+      </div>
     </div>
   );
 };
+
+export default PieChart1;
