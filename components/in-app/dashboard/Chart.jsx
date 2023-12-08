84226d8ab6135152c742a0b414bd39432c254de1
student <sbs.alexlips@gmail.comconfig --global user.name alexgit config --global user.email sbs.alexlips@gmail.com>
2023-12-08 10:08:18 +0100

    chart dashboard bearbeitet

--- components/in-app/dashboard/Chart.jsx
+++ components/in-app/dashboard/Chart.jsx
@@ -1,12 +1,45 @@
-import React, { useState, useEffect } from "react";
-import { Card, Col, Container, Row } from "react-bootstrap";
+import React from "react";
+import { AreaChart, Area, XAxis, YAxis, Tooltip, Legend, Label, CartesianGrid, defs, linearGradient, stop } from "recharts";
 
 export default function Chart() {
+  const data = [
+    { time: '10pm', visitors: 40 },
+    { time: '11pm', visitors: 20 },
+    { time: '12am', visitors: 80 },
+    { time: '1am', visitors: 20 },
+    { time: '2am', visitors: 100 },
+    { time: '3am', visitors: 80 },
+    { time: '4am', visitors: 60 },
+    { time: '5am', visitors: 40 },
+    { time: '6am', visitors: 20 },
+    { time: '7am', visitors: 0 },
+  ];
 
-     return (
-<Container>
-</Container>
+  return (
+    <div className="chart-container">
+      <AreaChart
+        width={600}
+        height={300}
+        data={data}
+        className="background-color-secondary chart-abstand mt-3"
+      >
+        <XAxis dataKey="time" />
+        <YAxis ticks={[0, 20, 40, 60, 80, 100]} />
+        <Tooltip />
+        <Legend />
 
+        <defs>
+          <linearGradient id="colorGradient" x1="25%" y1="0%" x2="75%" y2="0%">
+            <stop offset="0%" stopColor="#30D5C8" />
+            <stop offset="100%" stopColor="#e025fa" />
+          </linearGradient>
+        </defs>
 
-     );
+        <Area type="monotone" dataKey="visitors" stroke="none" fill="url(#colorGradient)" />
+
+        {/* Verschiebe das Label nach links oben über der Y-Achse */}
+        <Label value="visitors" position="insideTopLeft" angle={-90} dx={-20} dy={-20} />
+      </AreaChart>
+    </div>
+  );
 }
--- pages/app/dashboard.js
+++ pages/app/dashboard.js
@@ -6,8 +6,8 @@ export default function Dashboard() {
   return (
     <div>
        <CardsInfo/>
-        <PieChart/>
         <Chart/>
+        <PieChart/>
     </div>
   );
 }
