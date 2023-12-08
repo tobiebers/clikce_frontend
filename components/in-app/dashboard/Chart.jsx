345835cd40a7ddf8a256cac05b683d34b8a928f3
student <sbs.alexlips@gmail.comconfig --global user.name alexgit config --global user.email sbs.alexlips@gmail.com>
2023-12-08 11:16:09 +0100

    chart dashboard bearbeitet

--- components/in-app/dashboard/Chart.jsx
+++ components/in-app/dashboard/Chart.jsx
@@ -1,18 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AreaChart, Area, XAxis, YAxis, Tooltip, Legend, Label, CartesianGrid, defs, linearGradient, stop } from "recharts";
 
 export default function Chart() {
+  const [visitors1, setVisitors1] = useState(0);
+  const [visitors2, setVisitors2] = useState(0);
+  const [visitors3, setVisitors3] = useState(0);
+  const [visitors4, setVisitors4] = useState(0);
+  const [visitors5, setVisitors5] = useState(0);
+  const [visitors6, setVisitors6] = useState(0);
+  const [visitors7, setVisitors7] = useState(0);
+  const [visitors8, setVisitors8] = useState(0);
+  const [visitors9, setVisitors9] = useState(0);
+  const [visitors10, setVisitors10] = useState(0);
+
+  useEffect(() => {
+    const fetchChart = async () => {
+
+      try {
+        const response = await fetch('http://localhost:5000/fetch-chart');
+        const data = await response.json();
+        setVisitors1(data.visitors1);
+        setVisitors2(data.visitors2);
+        setVisitors3(data.visitors3);
+        setVisitors4(data.visitors4);
+        setVisitors5(data.visitors5);
+        setVisitors6(data.visitors6);
+        setVisitors7(data.visitors7);
+        setVisitors8(data.visitors8);
+        setVisitors9(data.visitors9);
+        setVisitors10(data.visitors10);
+
+      } catch (error) {
+        console.error('Fehler beim Abrufen der Daten vom Backend:', error);
+      }
+    };
+
+    fetchChart();
+  }, []);
+
   const data = [
-    { time: '10pm', visitors: 40 },
-    { time: '11pm', visitors: 20 },
-    { time: '12am', visitors: 80 },
-    { time: '1am', visitors: 20 },
-    { time: '2am', visitors: 100 },
-    { time: '3am', visitors: 80 },
-    { time: '4am', visitors: 60 },
-    { time: '5am', visitors: 40 },
-    { time: '6am', visitors: 20 },
-    { time: '7am', visitors: 0 },
+    { time: '10pm', visitors: visitors1 },
+    { time: '11pm', visitors: visitors2 },
+    { time: '12am', visitors: visitors3 },
+    { time: '1am', visitors: visitors4 },
+    { time: '2am', visitors: visitors5 },
+    { time: '3am', visitors: visitors6 },
+    { time: '4am', visitors: visitors7 },
+    { time: '5am', visitors: visitors8 },
+    { time: '6am', visitors: visitors9 },
+    { time: '7am', visitors: visitors10 }
   ];
 
   return (
