3a91259604b42975397484b869dd989fc4cffb4b
MaaB0I <dieterrorhenne@gmail.com>
2024-04-21 20:42:18 +0200

    Analytic Changes

--- components/in-app/analytics/ButtonGroup.jsx
+++ components/in-app/analytics/ButtonGroup.jsx
@@ -1,33 +1,6 @@
 import React, { useState, useCallback, useEffect } from 'react';
 import { useUsername } from 'pages/app/analytics.js';
 
-function ToggleSwitch({ onToggle }) {
-    const [isChecked, setIsChecked] = useState(false);
-
-    const handleToggle = useCallback(() => {
-        setIsChecked(false);
-        onToggle('instagram');
-    }, [onToggle]);
-
-    return (
-        <div className="toggle-switch-wrapper">
-            <input
-                type="checkbox"
-                id="toggle-switch"
-                className="toggle-switch-checkbox"
-                checked={isChecked}
-                onChange={handleToggle}
-                disabled={true}
-            />
-            <label htmlFor="toggle-switch" className="toggle-switch-container">
-                <div>Instagram</div>
-                {/* Füge eine Klasse hinzu, um TikTok optisch zu deaktivieren */}
-                <div className="disabled-option">TikTok</div>
-            </label>
-        </div>
-    );
-}
-
 function CustomDropdownMenu({ platform }) {
     const [accounts, setAccounts] = useState([]);
     const { onSelectUsername } = useUsername(); // Verwendung des Contexts
@@ -42,12 +15,8 @@ function CustomDropdownMenu({ platform }) {
             .catch(error => console.error('Error:', error));
     }, [platform]);
 
-    // Diese Funktion wird aufgerufen, wenn ein Account ausgewählt wird
     const handleSelectAccount = useCallback((username) => {
-        // Setzt den ausgewählten Benutzernamen im Kontext
         onSelectUsername(username);
-
-        // Sendet eine POST-Anfrage, um die Profilinformationen des Benutzers zu speichern
         fetch(`http://localhost:5000/save-instagram-profile-data`, {
             method: 'POST',
             headers: {
@@ -86,7 +55,6 @@ function CombinedControls() {
 
     return (
         <div className="controls-container">
-            <ToggleSwitch onToggle={setPlatform} />
             <CustomDropdownMenu platform={platform} />
         </div>
     );
