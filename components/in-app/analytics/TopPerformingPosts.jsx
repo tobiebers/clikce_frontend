4e789fdd9ec1bd6d474ade4676218ab18402c2f2
MaaB0I <dieterrorhenne@gmail.com>
2024-04-29 22:36:54 +0200

    Optische änderungen

--- components/in-app/analytics/TopPerformingPosts.jsx
+++ components/in-app/analytics/TopPerformingPosts.jsx
@@ -3,14 +3,12 @@ import React from 'react';
 const FlipCard = () => {
   return (
     <div className="flip-card">
-      <div className="flip-card-inner">
+      <div>
         <div className="flip-card-front">
           <img src="/PlaceHolderFLipCard.png" alt="Top-Post" style={{ width: '100%', height: '80%' }} />
           <h3 className="title">Top-Post</h3>
         </div>
         <div className="flip-card-back">
-          <h3 className="title">BACK</h3>
-          <p>Leave Me</p>
         </div>
       </div>
     </div>
